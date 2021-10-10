2ae9f36700811744ef289327da6305040c8279e7
Alex Shkil <alexhlins@gmail.com>
2021-10-10 21:49:42 +0300

    start to dev toolset and example structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
+int inputValue = 0;
+// messages with key code
+char key_code [136] = {0,0,  1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 0,0,0,0,
+                              0,0,1,0, 0,0,0,0, 0,0,0,0, 1,0,0,1, 0,1,1,1,
+                              0,1,0,1, 1,1,0,0, 1,0,1,1, 1,1,1,1, 1,1,1,1,
+                              1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1,
+                              1,1,1,1, 1,1,1,0, 1,0,0,0, 1,1,1,0, 1,0,1,0,
+                              0,1,0,1, 0,0,0,0, 0,0,1,1, 1,1,1,1, 1,1,1,1,
+                              1,1,1,1, 1,1,1,0, 1,1,1,1, 1,1};
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT); 
   pinMode(PIN0, INPUT);
-
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Hello!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int inputValue;
+  
   inputValue = digitalRead(PIN0);
   digitalWrite(LED_BUILTIN, inputValue);
-  Serial.write("Hello! \n");
+  Serial.println("1");
   delay(10);
 }
---
+++ tools/key2code.py
@@ -0,0 +1,48 @@
+# ----------------------------------------------------------------
+#
+# key2code tool kit
+# This tool kit helps to generate byte sequence to communicate with immobilizer system
+# or get key ID from given sequence
+#
+# Author: Alex Shkil aka HLinS
+#
+# ----------------------------------------------------------------
+
+
+# ----------------------------------------------------------------
+# Example dump data: [header] [key] [data_bcc] [stop_byte] [null_end]
+# ----------------------------------------------------------------
+
+header = 0x7F  # read-only RFID
+key = [0xF, 0xF, 0xB, 0x4, 0x5, 0x1, 0xA, 0x0,
+       0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
+data_bcc = []
+stop_byte = 0x7e
+null_end = [0x00, 0x00]
+
+
+def getReversedMessageHex2Bin(hex_message):
+    ''' Return'''
+    bin = ''
+    rev_bin = ''
+    for element in hex_message:
+        segment = format(element, '04b')
+        bin += segment
+        rev_bin = str([segment[i] for i in range(len(segment)-1, -1, -1)]).replace(
+            '[', '').replace('\'', '').replace(',', '').replace(']', '').replace(' ', '') + rev_bin
+    print(bin)
+    print(rev_bin)
+    return rev_bin
+
+
+def getKeyFromMessage(message):
+    result = list()
+    for i in range(0, len(message), 4):
+        result.append(
+            hex(int(message[i]+message[i+1]+message[i+2]+message[i+3], base=2)))
+    return result
+
+
+# getReversedMessageHex2Bin(key)
+print([hex(i) for i in key])
+print(getKeyFromMessage(getReversedMessageHex2Bin(key)))
